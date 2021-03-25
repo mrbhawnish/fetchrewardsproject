7383a278483046d5855110fc47b46a7bb0ee4ac6
bhawnishk1 <mrbhawnish@gmail.com>
2021-03-25 11:53:04 -0700

    Config files ready

---
+++ src/main/java/com/fetchrewards/demo/PayerpointsApplication.java
@@ -0,0 +1,16 @@
+package com.fetchrewards.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayerpointsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PayerpointsApplication.class,
+            args);
+    }
+
+}
---
+++ src/test/java/com/fetchrewards/demo/PayerpointsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fetchrewards.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayerpointsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
