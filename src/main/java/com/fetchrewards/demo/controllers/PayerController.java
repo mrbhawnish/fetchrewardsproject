9c9ffebd63d4594081d8de34af9d8fb089a3fde3
bhawnishk1 <mrbhawnish@gmail.com>
2021-03-25 17:36:57 -0700

    endpoints fully functional for payer, transaction

--- src/main/java/com/fetchrewards/demo/controllers/PayerController.java
+++ src/main/java/com/fetchrewards/demo/controllers/PayerController.java
@@ -1,9 +1,51 @@
 package com.fetchrewards.demo.controllers;
 
+import com.fetchrewards.demo.models.Payer;
+import com.fetchrewards.demo.repository.PayerRepository;
+import com.fetchrewards.demo.services.PayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 public class PayerController
 {
+    @Autowired
+    PayerService payerService;
+
+    @GetMapping(value = "/allpayers",
+        produces = {"application/json"})
+    public ResponseEntity<?> getAllPayers()
+    {
+
+        List<Payer> payersList = payerService.allPayers();
+        return new ResponseEntity<>(payersList, HttpStatus.OK);
+    }
+
+    @PostMapping(value="/newpayer",
+        consumes = {"application/json"}, produces = "application/json")
+    public ResponseEntity<?> createNewPayer(@Valid @RequestBody Payer newPayer)
+    {
+        newPayer.setPayerid(0);
+        newPayer = payerService.save(newPayer);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newPayerURI = ServletUriComponentsBuilder.fromCurrentRequestUri()
+            .path("{payerid}")
+            .buildAndExpand(newPayer.getPayerid())
+            .toUri();
+        responseHeaders.setLocation(newPayerURI);
+        return new ResponseEntity<>(newPayer, responseHeaders, HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/fetchrewards/demo/controllers/TransactionController.java
+++ src/main/java/com/fetchrewards/demo/controllers/TransactionController.java
@@ -1,5 +1,44 @@
 package com.fetchrewards.demo.controllers;
 
+
+import com.fetchrewards.demo.models.Payer;
+import com.fetchrewards.demo.models.Transaction;
+import com.fetchrewards.demo.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
 public class TransactionController
 {
+    @Autowired
+    TransactionService transService;
+
+    @PostMapping(value="{payer/{payerid}/newtransaction",
+        consumes = {"application/json"}, produces = "application/json")
+    public ResponseEntity<?> addTransaction(@PathVariable long payerid,
+                                            @Valid
+                                            @RequestBody
+                                                Transaction newTrans)
+    {
+        newTrans.setTransactionid(0);
+        newTrans = transService.save(payerid, newTrans);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newPayerURI = ServletUriComponentsBuilder.fromCurrentRequestUri()
+            .path("{payerid}")
+            .buildAndExpand(newTrans.getPayer().getPayerid())
+            .toUri();
+        responseHeaders.setLocation(newPayerURI);
+        return new ResponseEntity<>(newTrans, responseHeaders, HttpStatus.CREATED);
+    }
 }
